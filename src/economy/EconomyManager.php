783c791180b35a5cfe692f5527f86eaeb81032b0
i1xxy <134897452+i1xxy@users.noreply.github.com>
2023-08-30 23:02:08 +0300

    Create EconomyManager.php

---
+++ src/economy/EconomyManager.php
@@ -0,0 +1,33 @@
+<?php
+declare(strict_types=1);
+
+namespace BEDevs\BedrockEssentials\economy;
+
+class EconomyManager{
+  /*
+   * Method to insert new player column
+   */
+  public function insert(){
+    # Codes
+  }
+  /*
+   * Method to read player column
+   */
+  public function read(){
+    # Codes
+  }
+
+  /*
+   * Method to update player column
+   */
+  public function update(){
+    # Codes
+  }
+
+  /*
+   * Method to delete player column
+   */
+  public function delete(){
+    # Codes
+  }
+}
