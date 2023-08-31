c0f411f0cfbf7c9650fb699ec1bb8d7d747ea5e7
i1xxy <134897452+i1xxy@users.noreply.github.com>
2023-08-31 13:48:39 +0300

    Update EconomyManager.php

--- src/economy/EconomyManager.php
+++ src/economy/EconomyManager.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 namespace BEDevs\BedrockEssentials\economy;
 
-use pocketmine\Player;
+use pocketmine\player\Player;
 use pocketmine\utils\Config;
 
 class EconomyManager{
