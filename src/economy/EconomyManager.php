701b96a5b15f69d80267885129c34554fbf5f251
iMD14 <137671905+iMD14@users.noreply.github.com>
2023-08-31 13:06:08 +0300

    Update EconomyManager.php

--- src/economy/EconomyManager.php
+++ src/economy/EconomyManager.php
@@ -3,40 +3,65 @@ declare(strict_types=1);
 
 namespace BEDevs\BedrockEssentials\economy;
 
+use pocketmine\Player;
 use pocketmine\utils\Config;
 
 class EconomyManager{
-  /*
-   * Method to insert new player column
-   */
-  public function insert(){
-    # Codes
+  private $config;
+  
+  public function __construct(Config $config){
+    $this->config = $config;
   }
+  
+  /*
+   * Method to insert a new player column
+   */
+  public function insert(Player|string $player, int $value){
+    $playerName = $player instanceof Player ? $player->getName() : $player;
+    if (!$this->exists($playerName)) {
+      $this->config->set($playerName, $value);
+      $this->config->save();
+    }
+  }
+  
   /*
    * Method to read player column
    */
-  public function read(){
-    # Codes
+  public function read(Player|string $player){
+    $playerName = $player instanceof Player ? $player->getName() : $player;
+    if ($this->exists($playerName)) {
+      return $this->config->get($playerName);
+    }
+    return null;
   }
-
+  
   /*
    * Method to update player column
    */
-  public function update(){
-    # Codes
+  public function update(Player|string $player, int $value){
+    $playerName = $player instanceof Player ? $player->getName() : $player;
+    if ($this->exists($playerName)) {
+      $this->config->set($playerName, $value);
+      $this->config->save();
+    }
   }
-
+  
   /*
    * Method to delete player column
    */
-  public function delete(){
-    # Codes
+  public function delete(Player|string $player){
+    $playerName = $player instanceof Player ? $player->getName() : $player;
+    if ($this->exists($playerName)) {
+      $this->config->remove($playerName);
+      $this->config->save();
+    }
   }
   
   /*
    * Method to check if player column exists
    */
-  public function exists(){
-    # Codes
+  public function exists(Player|string $player){
+    $playerName = $player instanceof Player ? $player->getName() : $player;
+    return $this->config->exists($playerName);
   }
 }
