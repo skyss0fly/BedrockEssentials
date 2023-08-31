a77c8885f3e8f934ee74cf8fc5eff1bbdbf451b3
i1xxy <134897452+i1xxy@users.noreply.github.com>
2023-08-31 14:59:18 +0300

    Update AFKCommand.php

--- src/commands/AFKCommand.php
+++ src/commands/AFKCommand.php
@@ -4,6 +4,20 @@ declare(strict_types=1);
 
 namespace BEDevs\BedrockEssentials\commands;
 
-class AFKCommand{
-  
+use pocketmine\plugin\Plugin;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\utils\Config;
+use pocketmine\player\Player;
+
+class AFKCommand extends Command {
+  public function __construct(Plugin $plugin){
+    $this->setPermission("bedrockessentials.afk.use");
+    parent::__construct("afk", "Toggle \"Away from keyboard\" mode", "/afk <boolean: True/False>");
+    $this->plugin = $plugin;
+  }
+
+  public function execute(CommandSender $sender, string $commandLabel, array $args) : bool {
+    return True;
+  }
 }
