270be67f231e76c285714fbbadf02d49a529052c
iMD14 <137671905+iMD14@users.noreply.github.com>
2023-08-31 13:42:29 +0300

    Update Main.php

--- src/Main.php
+++ src/Main.php
@@ -8,12 +8,30 @@ use pocketmine\plugin\PluginBase;
 
 class Main extends PluginBase {
   public function onEnable(): void{
+    $this->CheckConfig()
     # Codes
     $this->getLogger()->info("Plugin Enabled");
   }
       
-  public function reloadEssentials(){
+  public function onDisable(): void{
     # Codes
     $this->getLogger()->info("Plugin Disabled");
+  }
+  
+  public function registerCommands(){
+    #Codes
+    
+  }
+  
+  public function CheckConfig(){
+    if(!file_exists($this->getDataFolder() . "config.yml")){
+        $this->saveDefaultConfig();
+    }
+    if(!is_dir($this->getDataFolder() . "languages")){
+        mkdir($this->->getDataFolder() . "languages");
+    }
+    if(!file_exists($this->getDataFolder() . "languages/english.properties")){
+        $this->saveResource("languages/english.properties");
+    }
   }
 }
