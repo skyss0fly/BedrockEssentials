21690a2805f82726fa636b6245270c39f3b0daa6
i1xxy <134897452+i1xxy@users.noreply.github.com>
2023-09-01 11:50:51 +0300

    Create FeedCommand.php

---
+++ src/commands/FeedCommand.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BEDevs\BedrockEssentials\commands;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\player\Player;
+use pocketmine\plugin\PluginBase;
+
+class FoodCommand extends Command {
+  private $plugin;
+  public function __construct(Plugin $plugin){
+    $this->setPermission("bedrockessentials.feed");
+    parent::__construct("feed", "Replenish player's hunger", "/feed");
+    $this->plugin = $plugin;
+  }
+
+  public function execute(CommandSender $sender, string $commandLabel, array $args) : bool {
+    if($sender instanceof Player){
+      $sender->setFood($sender->getMaxFood());
+      $sender->sendMessage("§6You have been fed");
+    } else {
+      $sender->sendMessage("This command can only be used by players.");
+    }
+  }
+}
