3c75194ed9c97864ae8ec187115cd8edb11f6042
i1xxy <134897452+i1xxy@users.noreply.github.com>
2023-09-01 12:07:28 +0300

    Update ItemCommand.php

--- src/commands/ItemCommand.php
+++ src/commands/ItemCommand.php
@@ -4,18 +4,15 @@ declare(strict_types=1);
 
 namespace BEDevs\BedrockEssentials\commands;
 
-use pocketmine\plugin\Plugin;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\utils\Config;
 use pocketmine\player\Player;
 
 class ICommand extends Command {
-  private $plugin;
-  public function __construct(Plugin $plugin){
+  public function __construct(){
     $this->setPermission("bedrockessentials.item");
     parent::__construct("item", "Give yourself a item", "/item <item> [amount]");
-    $this->plugin = $plugin;
   }
 
   public function execute(CommandSender $sender, string $commandLabel, array $args) : bool {
